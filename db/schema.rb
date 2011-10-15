6b676f896536849019ed2575e22f75e065f57bf7
Vinson Chuong <Vinson.Chuong@gmail.com>
2011-10-14 17:34:57 -0700

    Store data as text instead of string

--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111014022012) do
+ActiveRecord::Schema.define(:version => 20111015002921) do
 
   create_table "results", :force => true do |t|
-    t.string "data"
+    t.text "data", :limit => 255
   end
 
 end
